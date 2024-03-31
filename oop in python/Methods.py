c3625214e30888274c7a2e4857ce7006cf7290fa
irakozej <jeaniakoze68@gmail.com>
2024-03-31 10:19:42 +0200

    oop in py

---
+++ oop in python/Methods.py
@@ -0,0 +1,7 @@
+# Create a class Dog with a property voice = "Woof" and a method bark(self) which prints out voice property using self.
+
+class Dog:
+    voice = "Woof"
+
+    def bark(self):
+        print(self.voice)
---
+++ oop in python/Objects.py
@@ -0,0 +1,8 @@
+#Create a class Person and add name = "Lord" as a variable in it.
+
+#Initiate an object lord with class Person and print the name within that Person object named lord
+
+class Person:
+    name = "Lord"
+lord = Person()
+print(lord.name)
