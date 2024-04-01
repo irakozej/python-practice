328a7c3fa7eaeabb8f3fc069f98cafc9f999c17d
irakozej <jeaniakoze68@gmail.com>
2024-04-01 23:11:53 +0200

    ..

---
+++ oop in python/constructor.py
@@ -0,0 +1,6 @@
+# Create a class Car with __init__() method that takes manufacturer & build_date as parameters & saves it as class variable to self.
+
+class Car:
+    def __init__(self, manufacturer, build_date):
+        self.manufacturer = manufacturer
+        self.build_date = build_date
