9e5ac4f2178f905989ea94668339c2336dfa6d6f
irakozej <jeaniakoze68@gmail.com>
2024-04-07 21:39:18 +0200

    nope

---
+++ oop in python/encapsulation/getterMethod.py
@@ -0,0 +1,16 @@
+class Capsule:
+    test = True
+
+    def __init__(self, value1):
+        self.__value1 = value1
+
+    ### Add your code below this line...
+    def get_value(self):
+        return self.__value1
+    
+
+### DO NOT CHANGE BELOW THIS LINE
+
+def test_answer(value1):
+    obj = Capsule(value1)
+    return obj.get_value()
