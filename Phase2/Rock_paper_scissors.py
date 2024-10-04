e076bb2ffcf3e95e69fc6255dbe2bc17e6176024
irakozej <jeaniakoze68@gmail.com>
2024-10-04 16:31:50 +0200

    modified the filr

--- Phase2/Rock_paper_scissors.py
+++ Phase2/Rock_paper_scissors.py
@@ -5,27 +5,30 @@ print("Welcome to Rock, Paper, Scissors game")
 print("You have to play with the computer")
 print("Enter your choice: Rock, Paper or Scissors")
 user = input("Enter your choice: ")
-computer = random.choice(["Rock", "Paper", "Scissors"])
+computer = random.choice(list(choice))
 
 print(f"Computer choice is: {computer}")
 
-if user == computer:
-    print("It's a tie")
-elif user == "Rock":
-    if computer == "paper":
-        print("computer wins")
+while True:
+    if user == computer:
+        print("It's a tie")
+        break
+    elif user == "Rock":
+        if computer == "Paper":
+            print("computer wins")
+        else:
+            print("You win")
+    elif user == "Paper":
+        if computer == "Scissors":
+            print("computer wins")
+        else:
+            print("You win")
+    elif user == "Scissors":
+        if computer == "Rock":
+            print("computer wins")
+        else:
+            print("You win")
     else:
-        print("You win")
-elif user == "Paper":
-    if computer == "scissors":
-        print("computer wins")
-    else:
-        print("You win")
-elif user == "Scissors":
-    if computer == "rock":
-        print("computer wins")
-    else:
-        print("You win")
-else:
-    print("Invalid choice")
+        print("Invalid choice")
 
+    break
